512e89885dccbae57524786305af65f7c08db340
Fernando Mendes <fernando.mendes@myprofitweb.com>
2025-10-17 11:47:21 -0300

    Initial commit

---
+++ src/light_controller.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+
+class LightController:
+    def __init__(self, pin=17):
+        self.pin = pin
+        self.light_state = False  # False means off, True means on
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def turn_on(self):
+        if not self.light_state:
+            self.light_state = True
+            GPIO.output(self.pin, GPIO.HIGH)
+            print("Light turned ON")
+
+    def turn_off(self):
+        if self.light_state:
+            self.light_state = False
+            GPIO.output(self.pin, GPIO.LOW)
+            print("Light turned OFF")
+
+    def get_light_state(self):
+        return self.light_state
+
+    def cleanup(self):
+        GPIO.cleanup()
