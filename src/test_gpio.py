dd111c880e5fa9b02e05a820a97d8d98527d9e1d
Fernando Mendes <fernando.mendes@myprofitweb.com>
2025-10-18 13:14:11 -0300

    feat: Alteração

--- src/test_gpio.py
+++ src/test_gpio.py
@@ -1,6 +1,6 @@
 # Teste manual: liga 2s, desliga 2s e imprime estado final
 import time
-from src.light_controller import LightController
+from light_controller import LightController
 
 lc = LightController(pin=12)  # confirme pin (BCM)
 lc.turn_on()
